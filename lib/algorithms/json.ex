33f6f7ab1adb4234e036691a4129dea4e0e6e99a
Zieghailo <isakov.m@gmail.com>
2015-06-04 20:42:22 +0200

    Added type printing and type stack printing for better visualization

--- lib/algorithms/json.ex
+++ lib/algorithms/json.ex
@@ -42,3 +42,71 @@ defmodule Json do
 
 end
 
+
+defmodule TypeStack do
+
+  def print(element, space \\ 0)
+
+  def print(tuple, space) when is_tuple(tuple) do
+    IO.write indent space 
+    IO.puts "tuple" 
+    Enum.map(Tuple.to_list(tuple), fn x -> print(x, space+1) end)
+  end
+
+  def print(list, space) when is_list(list) do
+    IO.write indent space
+    IO.puts "list"
+    Enum.map(list, fn x -> print(x, space+1) end)
+  end
+  
+  def print(num, space) when is_number(num) do
+    IO.write indent space
+    IO.puts "number"
+  end
+
+  def print(atom, space) when is_atom(atom) do
+    IO.write indent space  
+    IO.puts "atom"
+  end
+
+  def print(string, space) when is_bitstring(string) do
+    IO.write indent space
+    IO.puts "string"
+  end
+
+  def print(any, space) do
+    raise ArgumentError, message: "Unmatched data type!"
+  end
+
+  defp indent(num) do
+    String.duplicate("  ", num)
+  end
+end
+
+
+defmodule Typer do
+
+  def print(element) when is_tuple(element) do
+    IO.write "{ "
+    Enum.map(Tuple.to_list(element), fn x-> IO.write(get_type(x) <> ", ") end)
+    IO.puts " }"
+    Enum.map(Tuple.to_list(element), fn x-> print(x) end)
+  end
+
+  def print(element) when is_list(element) do
+    IO.write "[ "
+    Enum.map(element, fn x-> IO.write(get_type(x) <> ", ") end)
+    IO.puts " ]"
+    Enum.map(element, fn x-> print(x) end)
+  end
+
+  def print(element) do
+  end
+
+  def get_type(element) when is_tuple(element), do: "tuple"
+  def get_type(element) when is_list(element), do: "list"
+  def get_type(element) when is_atom(element), do: "atom"
+  def get_type(element) when is_bitstring(element), do: "string"
+  def get_type(element) when is_number(element), do: "number" 
+
+end
