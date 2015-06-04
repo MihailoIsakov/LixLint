038ef9af79131faf4dc3fe8755b58b4a6b576826
Zieghailo <isakov.m@gmail.com>
2015-06-04 20:46:38 +0200

    Examle for Typer and TypeStack in main.ex

--- lib/algorithms/main.ex
+++ lib/algorithms/main.ex
@@ -4,3 +4,9 @@ json = Json.encode(str)
 {:ok, file} = File.open "out.json", [:write]
 IO.binwrite file, json 
 File.close file
+
+# Typer doc
+Typer.print str
+
+# Type stack doc
+TypeStack.print str
